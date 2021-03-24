43dfc01971be3e6f3a1633e12ae0546ea2cd1acf
jericho1798 <vladislav.bulychev98@yandex.ru>
2021-03-25 01:19:33 +0300

    pt_jdbc

---
+++ src/main/java/vsb/tacocloud/data/IngredientRepository.java
@@ -0,0 +1,12 @@
+package vsb.tacocloud.data;
+
+import vsb.tacocloud.Ingredient;
+
+public interface IngredientRepository {
+
+    Iterable<Ingredient> findAll();
+
+    Ingredient findOne(String id);
+
+    Ingredient save(Ingredient ingredient);
+}
--- src/main/java/vsb/tacocloud/web/DesignTacoController.java
+++ src/main/java/vsb/tacocloud/web/DesignTacoController.java
@@ -1,20 +1,21 @@
 package vsb.tacocloud.web;
+
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import vsb.tacocloud.Ingredient;
 import vsb.tacocloud.Ingredient.Type;
 import vsb.tacocloud.Taco;
+import vsb.tacocloud.data.IngredientRepository;
 
 import javax.validation.Valid;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+
 
 //import sun.text.normalizer.NormalizerBase;
 
@@ -22,21 +23,19 @@ import java.util.stream.Collectors;
 @Slf4j
 @Controller
 @RequestMapping("/design")
+@SessionAttributes("order")
 public class DesignTacoController {
+
+    private final IngredientRepository ingredientRepository;
+
+    @Autowired
+    public DesignTacoController(IngredientRepository ingredientRepository) {
+        this.ingredientRepository = ingredientRepository;
+    }
     @ModelAttribute
-    public void addIngredientsToModel(Model model) {
-        List<Ingredient> ingredients = Arrays.asList(
-                new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP),
-                new Ingredient("COTO", "Corn Tortilla", Ingredient.Type.WRAP),
-                new Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN),
-                new Ingredient("CARN", "Carnitas", Ingredient.Type.PROTEIN),
-                new Ingredient("TMTO", "Diced Tomatoes", Ingredient.Type.VEGGIES),
-                new Ingredient("LETC", "Lettuce", Ingredient.Type.VEGGIES),
-                new Ingredient("CHED", "Cheddar", Ingredient.Type.CHEESE),
-                new Ingredient("JACK", "Monterrey Jack", Ingredient.Type.CHEESE),
-                new Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE),
-                new Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE)
-        );
+    public void addIngredientsToModel (Model model) {
+        List<Ingredient> ingredients = new ArrayList<>();
+        ingredientRepository.findAll().forEach(ingredients::add);
         Type[] types = Ingredient.Type.values();
         for(Type type : types) {
             model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
